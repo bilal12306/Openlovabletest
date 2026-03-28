b510515e4d6799a4d90d6ab3b1d96d006098154f
bilal12306 <bilalnadeem6767@gmail.com>
2026-03-28 23:49:14 +0500

    Add main components to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,26 @@
+import IntroScene from '@/components/IntroScene'
+import HeroSection from '@/components/HeroSection'
+import SignatureFusion from '@/components/SignatureFusion'
+import WhyFoodBrew from '@/components/WhyFoodBrew'
+import FeaturedMenu from '@/components/FeaturedMenu'
+import ThreeCarousel from '@/components/ThreeCarousel'
+import AboutSection from '@/components/AboutSection'
+import Testimonials from '@/components/Testimonials'
+import CTASection from '@/components/CTASection'
 
+export default function Home() {
+  return (
+    <main>
+      <IntroScene />
+      <HeroSection />
+      <SignatureFusion />
+      <WhyFoodBrew />
+      <FeaturedMenu />
+      <ThreeCarousel />
+      <AboutSection />
+      <Testimonials />
+      <CTASection />
+    </main>
+  )
+}
+  
