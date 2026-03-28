846783acb4e09d3c7b2c0dad194d558ec2a94880
bilal12306 <bilalnadeem6767@gmail.com>
2026-03-29 00:03:35 +0500

    Update hooks.ts

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,1 +1,32 @@
 
+import { useState, useEffect } from 'react'
+
+export function useSessionStorage(key: string, initialValue: any) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      if (typeof window !== 'undefined') {
+        const item = window.sessionStorage.getItem(key)
+        return item ? JSON.parse(item) : initialValue
+      }
+      return initialValue
+    } catch (error) {
+      console.error(error)
+      return initialValue
+    }
+  })
+
+  const setValue = (value: any) => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value
+      setStoredValue(valueToStore)
+      if (typeof window !== 'undefined') {
+        window.sessionStorage.setItem(key, JSON.stringify(valueToStore))
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return [storedValue, setValue]
+}
+  
